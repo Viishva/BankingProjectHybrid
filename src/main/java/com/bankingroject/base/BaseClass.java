408aa5335aa008ce1824518953d3132a80dafaaf
Viishva <ekshingevishvajeet@gmail.com>
2023-01-19 07:55:51 +0530

    my first commit

---
+++ src/main/java/com/bankingroject/base/BaseClass.java
@@ -0,0 +1,19 @@
+package com.bankingroject.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	public static void main(String[] args) {
+		 System.setProperty("webdriver.chrome.driver","D:\\Selenium\\chromedriver.exe");
+	     WebDriver driver = new ChromeDriver();     
+		 
+	     driver.get("https://demo.guru99.com/V1/index.php");
+	     
+		
+		
+		
+	}
+	
+
+}
